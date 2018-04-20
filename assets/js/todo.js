e6970823909a15c9e6a2a6fe756d91a6e1603b88
Biyun Wu <beenwu2016@gmail.com>
2018-04-20 11:17:03 -0400

    Add styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,5 @@
 //Global variable
-var deleteButton = "<span>X</span> ";
+var deleteButton = "<span><i class='fas fa-trash-alt'></i></span>";
 
 // Change the style of calendar records when clicked.
 $("ul").on("click", "li", function(){
@@ -8,7 +8,7 @@ $("ul").on("click", "li", function(){
 
 // Delete the record if the delete button is triggered.
 $("ul").on("click", "span", function(event){
-  $(this).parent().fadeOut(400, function(){
+  $(this).parent().fadeOut(300, function(){
     $(this).remove();
   });
   // Get rid of events bubble.
@@ -23,4 +23,8 @@ $("input[type='text']").keypress(function(event){
     $(this).val("");
     $("ul").append("<li>" + deleteButton + input + "</li>");
   }
+});
+
+$("span:first").click("on", function(){
+  $("input[type='text']").fadeToggle();
 });
